efbbf482e669054db51547eb3a311139f9bbc33f
Christian Brotherson <christian.brotherson@gmail.com>
2017-11-20 14:56:37 -0700

    Set up a custom redirect to catch an incorrect path

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
 
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
+  get "error", to: "pages#error"
+
+  get "blog", to: redirect("https://ckb-portfolio.herokuapp.com/blogs")
 
   resources :projects
 
   root 'pages#home'
+
+  get "*path", to: redirect("/error")
 end
