9f0090a7d24e20b5ef87d24c6bbe0eb72c26583d
Christian Brotherson <christian.brotherson@gmail.com>
2017-11-20 21:19:19 -0700

    Removed column using Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121041452) do
+ActiveRecord::Schema.define(version: 20171121041831) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
