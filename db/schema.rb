0d042de78a6d1cc53874b310bb06f17e0f4d9687
Christian Brotherson <christian.brotherson@gmail.com>
2017-11-20 21:16:17 -0700

    Changed the data type for stage in projects table in schema through a migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121041239) do
+ActiveRecord::Schema.define(version: 20171121041452) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20171121041239) do
     t.decimal  "percent_complete"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-    t.integer  "stage"
+    t.string   "stage"
   end
 
   create_table "tasks", force: :cascade do |t|
