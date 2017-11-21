a8693b784c851f269106b483e5a135ebe3c9e647
Christian Brotherson <christian.brotherson@gmail.com>
2017-11-20 21:13:40 -0700

    Added a column to the projects schema table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121035732) do
+ActiveRecord::Schema.define(version: 20171121041239) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171121035732) do
     t.decimal  "percent_complete"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "stage"
   end
 
   create_table "tasks", force: :cascade do |t|
